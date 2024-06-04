44d37bf4952a0570935e821a5c7fa458bfe8580a
ahmedrayyan395 <116526799+ahmedrayyan395@users.noreply.github.com>
2024-06-05 00:38:19 +0300

    Update getProducts.py

--- getProducts.py
+++ getProducts.py
@@ -81,7 +81,7 @@ def send_product_data_to_telegram():
                 print("-" * 50)
 
         bot_token = "6958486146:AAFtYb_TaInJtSSFevXDn39BCssCzj4inV4"
-        chat_id = "@desertfs"
+        chat_id = "@dezertts"
         telegram_api_url = f"https://api.telegram.org/bot{bot_token}/sendPhoto"
 
         for product_data in product_data_list:
